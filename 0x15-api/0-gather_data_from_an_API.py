4c311f8e378d48fca1d1de00db4fcaf7206f9792
Tasie Ibiam <tasieibiam28@gmail.com>
2023-05-30 07:42:50 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Returns information about am employee todo list progress using their ID"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    employee = requests.get(url + "/users" + "/{}".format(sys.argv[1])).json()
+    employee_todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+    completed_tasks = [task.get("title") for task in employee_todos if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(completed_tasks), len(employee_todos)))
+    [print("\t {}".format(done)) for done in completed_tasks]

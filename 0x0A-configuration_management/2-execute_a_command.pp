6ef5f57dd705d23fae03285802794fb230a4a33d
Tasie Ibiam <tasieibiam28@gmail.com>
2024-03-23 06:59:30 +0100

    puppet show

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Creates a manifest that kills a process named killmenow.
 exec { 'pkill killmenow':
   command => '/usr/bin/pkill killmenow',
-  path    => '/usr/bin',
   onlyif  => '/usr/bin/pgrep killmenow',
 }

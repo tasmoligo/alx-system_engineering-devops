190432bd532ac852bfab0d74d4b75a5ae4638af8
Tasie Ibiam <tasieibiam28@gmail.com>
2024-03-23 07:11:22 +0100

    puppet show

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
-# Installs flask version 2.1.0
+# Instrall flask version 2.1.0
 package { 'python3-pip':
   ensure => installed,
 }
 
 exec { 'install_flask':
-  command  => 'pip3 install flask',
-  path     => '/usr/bin/',
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+  command => 'pip3 install Flask==2.1.0',
+  path    => '/usr/bin/',
+  unless  => 'pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
+

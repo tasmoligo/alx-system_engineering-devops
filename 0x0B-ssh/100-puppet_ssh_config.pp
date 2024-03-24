294d6deda5d4cb05f2f0d7ef287e17f9bbe604b6
Tasie Ibiam <tasieibiam28@gmail.com>
2024-03-24 15:18:42 +0100

    Puppetgs!

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# set up your client SSH configuration file so that you
+# can connect to a server without typing a password.
+
+file_line { 'use private key':
+    ensure  => present,
+    line    => 'IdentityFile ~/.ssh/school',
+    path    => '/etc/ssh/sshd_config',
+}
+
+file_line { 'disable password':
+    ensure  => present,
+    line    => 'PasswordAuthentication no',
+    path    => '/etc/ssh/sshd_config',
+}

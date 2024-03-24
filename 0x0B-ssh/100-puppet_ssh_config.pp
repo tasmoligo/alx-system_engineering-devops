09a774718f94b845cb065e66a20ba4d94edfaabf
Tasie Ibiam <113722054+tasmoligo@users.noreply.github.com>
2024-03-24 21:33:57 +0100

    Puppet!!!

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,19 +4,11 @@
 file_line { 'use private key':
     ensure  => present,
     line    => 'IdentityFile ~/.ssh/school',
-<<<<<<< HEAD
     path    => '/etc/ssh/sshd_config',
-=======
-    path    => '/etc/ssh/ssh_config',
->>>>>>> 256e07974f8b5a625b6e4008a79314357b40f85d
 }
 
 file_line { 'disable password':
     ensure  => present,
     line    => 'PasswordAuthentication no',
-<<<<<<< HEAD
-    path    => '/etc/ssh/sshd_config',
-=======
     path    => '/etc/ssh/ssh_config',
->>>>>>> 256e07974f8b5a625b6e4008a79314357b40f85d
 }

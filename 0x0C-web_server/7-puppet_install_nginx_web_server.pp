6318529c6c8ef0ada108de6d990cd571a52f9c65
Tasie Ibiam <tasieibiam28@gmail.com>
2024-04-06 16:09:30 +0100

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,17 +3,17 @@ package { 'nginx':
         ensure => present;
 }
 exec { 'install nginx':
-        command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+        command  => 'sudo apt-get update; sudo apt-get -y install nginx',
         provider => shell,
 }
 exec { 'root file':
-        command  => 'sudo echo "Hello World!" > /var/www/html/index.html',
+        command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
         provider => shell,
 }
-exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https://github.com/tasmoligo\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\nlocation \/redirect_me {\n\treturn 301 https:\/\/github.com;\n}/" /etc/nginx/sites-available/default':
         provider => shell,
 }
 exec { 'start nginx':
-        command  => 'sudo service nginx start',
+        command  => 'sudo service nginx restart',
         provider => shell,
 }

2b8043388d7953a548380ea826b1df07057f5d62
Tasie Ibiam <tasieibiam28@gmail.com>
2024-04-26 02:09:18 +0100

    API

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Returns information about an employee's TODO list progress based on
+their id using a REST API
+"""
+
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    emp_Id = sys.argv[1]
+    mainUrl = "https://jsonplaceholder.typicode.com/users"
+    url = mainUrl + "/" + emp_Id
+    res = requests.get(url)
+    emp_username = res.json().get('username')
+    todo_url = url + "/todos"
+    res = requests.get(todo_url)
+    tasks = res.json()
+    emp_data = {emp_Id: []}
+    for task in tasks:
+        emp_data[emp_Id].append({"task": task.get("title"), 
+            "completed": task.get("completed"),
+            "username": emp_username
+        })
+    with open('{}.json'.format(emp_Id), "w") as f:
+        json.dump(emp_data, f)

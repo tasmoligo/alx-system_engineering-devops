8fe1726a3291617a9251141c482388895d274af9
Tasie Ibiam <tasieibiam28@gmail.com>
2024-08-16 14:39:46 +0100

    fixing 500 error of apache with puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix 500 error of apache
 
-exec { 'filename typo error':
-	command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-	path => '/bin:/bin/usr'
+exec { 'correct_name' :
+	command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+	path 	=> ['/bin,/usr/bin']
 }

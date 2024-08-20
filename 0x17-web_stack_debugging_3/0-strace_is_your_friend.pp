2cfebf9bcdf486cfd74d04fd6311911aeec7ae62
Tasie Ibiam <tasieibiam28@gmail.com>
2024-08-20 02:29:52 +0100

    fixing 500 error of apache with puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet code to fix 500 error of apache.
 
 exec { 'correct_name':
-	command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-	path    => ['/bin', '/usr/bin'],
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin'],
 }

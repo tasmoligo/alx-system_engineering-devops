1a077d6d5cfeba896197cf541a9dd0269c7daada
Tasie Ibiam <tasieibiam28@gmail.com>
2024-09-24 16:05:24 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Total number of subscribers for a given subreddit
+number of subscribers for a given subreddit
 """
 
 from requests import get
@@ -8,13 +8,14 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers
+    function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     """
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 128.0.6613.84'}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
